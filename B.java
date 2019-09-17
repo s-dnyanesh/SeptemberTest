0ddd380c68a73d9811f3aa241216d9e8af2c03b9
somwanshi.dnyaneshwar@gmail.com <Mahatma@352>
2019-09-17 23:05:00 +0530

    sum of two numbers

--- B.java
+++ B.java
@@ -1,5 +1,9 @@
-class B{
-	System.out.println("I m new here !")
+
+public class SumOfTwo {
+	public static void main(String[] args) {
+	System.out.println(24+26);
+	System.out.println(50/3);
+	}
+	
 
 }
-
